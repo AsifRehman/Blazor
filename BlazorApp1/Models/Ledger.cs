3c6da6b615da631100cc202957c804792fe111bb
Asif Rehman <asif.pro@gmail.com>
2020-07-14 11:41:35 +0500

    Add project files.

---
+++ BlazorApp1/Models/Ledger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class Ledger
+    {
+        public int Id { get; set; } 
+        public string TType { get; set; }
+        public int VocNo { get; set; }
+        public int SrNo { get; set; }
+        public DateTime Date { get; set; } = DateTime.Today;
+        public int PartyID { get; set; }
+        public string Description { get; set; }
+        public int NetDebit { get; set; }
+        public int NetCredit { get; set; }
+    }
+}
---
+++ DataLibrary/DataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection con = new SqlConnection(connectionString))
+            {
+                var rows = await con.QueryAsync<T>(sql, parameters);
+                return rows.ToList();
+            }
+        }
+
+        public Task SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection con = new SqlConnection(connectionString))
+            {
+                return con.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
