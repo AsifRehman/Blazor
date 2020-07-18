e6fb3210a0cbc3894307a14b60ea79d61a103e6b
Asif Rehman <asifrehman007@hotmail.com>
2020-07-18 15:39:32 +0500

    parties page

---
+++ BlazorApp1/Models/Party.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class Party
+    {
+        public int Id { get; set; }
+        public string PartyName { get; set; }
+        public int PartyTypeID { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID#:{Id.ToString()},PartyName:{PartyName}, PartyTypeID:{PartyTypeID.ToString()}";
+        }
+    }
+}
